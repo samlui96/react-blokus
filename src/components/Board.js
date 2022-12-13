2e29a12f64c3aefdf8de62d1fb80558bff62e8b6
samlui <luipaksum@hotmail.com>
2022-12-12 23:26:58 -0500

    player take turn update

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,24 +1,39 @@
 import React from "react";
 // import useStyles from "./Board.style";
-import { Row } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { useSelector } from "react-redux";
+import { Row, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useDispatch, useSelector } from "react-redux";
+import { changePlayer } from "../redux/playerSlice";
 import Grid from "./Grid";
 import Tiles from "./Tiles";
 // import POLYOMINOES from './constant'
 
 const Board = () => {
-  const tileState = useSelector(state => state.tile)
-  const playerState = useSelector(state => state.player)
+  const dispatch = useDispatch();
+  const tileState = useSelector((state) => state.tile);
+  const playerState = useSelector((state) => state.player);
 
   return (
     <div>
-        <Row>
-          <Grid playerState={playerState} tileState={tileState} />
-        </Row>
-        <Row>
-          <Tiles playerState={playerState} tileState={tileState}/>
-        </Row>
+      <Row>
+        <Grid playerState={playerState} tileState={tileState} />
+      </Row>
+      <Row>
+        <Tiles playerState={playerState} tileState={tileState} />
+      </Row>
+      <Row>
+        <div className="mb-2">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={(e) => {dispatch(
+              changePlayer(playerState.curPlayer < 3 ? playerState.curPlayer + 1 : 0)
+            )}}
+          >
+            Skip
+          </Button>
+        </div>
+      </Row>
     </div>
   );
 };
